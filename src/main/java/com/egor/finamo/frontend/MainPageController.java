63ae8732758e0adbb66a8afa8581291fba23808e
crusadervii <egor.chervonikov@yandex.ru>
2025-08-30 17:48:20 +0300

    init commit

---
+++ src/main/java/com/egor/finamo/frontend/MainPageController.java
@@ -0,0 +1,15 @@
+package com.egor.finamo.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("finamo/home")
+public class MainPageController {
+
+    @GetMapping()
+    public String getHomePage() {
+        return "home";
+    }
+}
